68fd63eb3fa9ab897d7dd3077074c92bddc65e9b
Konstantin Zagulin <konstzv@gmail.com>
2018-10-10 23:17:24 +0300

    Added code from homework

--- app/src/main/java/com/zagulin/threads4/MainActivity.kt
+++ app/src/main/java/com/zagulin/threads4/MainActivity.kt
@@ -4,5 +4,24 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    public override fun onStart() {
+        super.onStart()
+        Thread(LeftLeg()).start()
+        Thread(RightLeg()).start()
+    }
+
+    private inner class LeftLeg : Runnable {
+        private val isRunning = true
+        override fun run() {
+            while (isRunning) println("Left step")
+        }
+    }
+
+    private inner class RightLeg : Runnable {
+        private val isRunning = true
+        override fun run() {
+            while (isRunning) println("Right step")
+        }
+    }
 
 }
