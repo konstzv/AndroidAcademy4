1f1a863caa0e973e941076fc5a2f982658deafae
Konstantin Zagulin <konstzv@gmail.com>
2018-10-12 11:31:00 +0300

    Made threads synchronized Added locker object

--- app/src/main/java/com/zagulin/threads4/MainActivity.kt
+++ app/src/main/java/com/zagulin/threads4/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.zagulin.threads4
 
 import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val locker = Locker()
+
     public override fun onStart() {
         super.onStart()
         Thread(LeftLeg()).start()
@@ -11,17 +13,59 @@ class MainActivity : AppCompatActivity() {
     }
 
     private inner class LeftLeg : Runnable {
-        private val isRunning = true
+
         override fun run() {
-            while (isRunning) println("Left step")
+
+            synchronized(locker) {
+                while (locker.isRunning) {
+                    while (locker.status == Locker.RIGHT_LEG_NEXT) {
+                        locker.wait()
+                    }
+                    println("Left step")
+                    locker.status = Locker.RIGHT_LEG_NEXT
+                    locker.notifyAll()
+                }
+            }
         }
+
     }
 
     private inner class RightLeg : Runnable {
-        private val isRunning = true
+
         override fun run() {
-            while (isRunning) println("Right step")
+            synchronized(locker) {
+
+                while (locker.isRunning) {
+                    while (locker.status == Locker.LEFT_LEG_NEXT) {
+                        locker.wait()
+                    }
+                    println("Right step")
+                    locker.status = Locker.LEFT_LEG_NEXT
+                    locker.notifyAll()
+                }
+            }
+
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        locker.isRunning = false
+    }
+
+    class Locker : Object() {
+
+        companion object {
+            const val LEFT_LEG_NEXT = 0
+            const val RIGHT_LEG_NEXT = 1
+        }
+
+        @Volatile
+        var status = LEFT_LEG_NEXT
+
+        @Volatile
+        var isRunning = true
+
     }
 
 }
