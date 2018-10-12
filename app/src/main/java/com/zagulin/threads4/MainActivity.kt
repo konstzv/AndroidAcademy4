71f5a726af2d26e8d325ee97855bdbd7f01bd814
Konstantin Zagulin <konstzv@gmail.com>
2018-10-12 11:46:06 +0300

    locker was renamed Added layout and textview

--- app/src/main/java/com/zagulin/threads4/MainActivity.kt
+++ app/src/main/java/com/zagulin/threads4/MainActivity.kt
@@ -1,47 +1,59 @@
 package com.zagulin.threads4
 
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val locker = Locker()
+    companion object {
+        private const val LEFT_STEP = "Left step"
+        private const val RIGHT_STEP = "Right step"
+    }
+
+
+    private val legsController = LegsController()
 
     public override fun onStart() {
         super.onStart()
+        setContentView(R.layout.activity_main)
         Thread(LeftLeg()).start()
         Thread(RightLeg()).start()
     }
+
 
     private inner class LeftLeg : Runnable {
 
         override fun run() {
 
-            synchronized(locker) {
-                while (locker.isRunning) {
-                    while (locker.status == Locker.RIGHT_LEG_NEXT) {
-                        locker.wait()
+            synchronized(legsController) {
+                while (legsController.isRunning) {
+                    while (legsController.status == LegsController.RIGHT_LEG_NEXT) {
+                        legsController.wait()
                     }
-                    println("Left step")
-                    locker.status = Locker.RIGHT_LEG_NEXT
-                    locker.notifyAll()
+                    println(LEFT_STEP)
+                    runOnUiThread { activity_main_text_view.text = LEFT_STEP }
+                    legsController.status = LegsController.RIGHT_LEG_NEXT
+                    legsController.notifyAll()
                 }
             }
         }
 
     }
 
+
     private inner class RightLeg : Runnable {
 
         override fun run() {
-            synchronized(locker) {
+            synchronized(legsController) {
 
-                while (locker.isRunning) {
-                    while (locker.status == Locker.LEFT_LEG_NEXT) {
-                        locker.wait()
+                while (legsController.isRunning) {
+                    while (legsController.status == LegsController.LEFT_LEG_NEXT) {
+                        legsController.wait()
                     }
-                    println("Right step")
-                    locker.status = Locker.LEFT_LEG_NEXT
-                    locker.notifyAll()
+                    println(RIGHT_STEP)
+                    runOnUiThread { activity_main_text_view.text = RIGHT_STEP }
+                    legsController.status = LegsController.LEFT_LEG_NEXT
+                    legsController.notifyAll()
                 }
             }
 
@@ -50,10 +62,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        locker.isRunning = false
+        legsController.isRunning = false
     }
 
-    class Locker : Object() {
+    class LegsController : Object() {
 
         companion object {
             const val LEFT_LEG_NEXT = 0
